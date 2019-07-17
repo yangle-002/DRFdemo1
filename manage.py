9c1efeabd9a67fd8a38be5901433d435bb133122
yangle <2462409058@qq.com>
2019-07-17 09:50:39 +0800

    test1

--- manage.py
+++ manage.py
@@ -3,6 +3,7 @@ import os
 import sys
 
 if __name__ == "__main__":
+    print("test1")
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "DRFdemo1.settings")
     try:
         from django.core.management import execute_from_command_line
